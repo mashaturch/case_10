f0b63d04ba71fc1abf31f7ccb4b0f86f414f68e0
mashaturch <s1yguvzg>
2021-09-15 23:57:23 +0700

    main_slovar

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ import sys
 import ru_local
 
 file = open('input.txt', encoding='utf-8')
+azs = open('azs.txt', encoding='utf-8')
 
-price = {'АИ-80': 32, 'АИ-92': 46, 'АИ-95': 49, 'АИ-98': 59}
+price = {'АИ-80': 32, 'АИ-92': 46, 'АИ-95': 49, 'АИ-98': 59} #цены на бензин
+petrol_with_machine = {} #словарь номер автомата + какие там есть бензины
+machine_with_set_time = {} #словарь с номером автомата + до скольких машина там находится
+machine_with_max_quantity = {} #словарь с номером автомата + макс очередь
 
-station = {1: 0, 2: 0, 3: 0}
+
+for line in azs:
+
+    data_input = list(line.split())
+
+    machine = data_input[0]
+    max_quantity = data_input[1]
+    petrols = list(data_input[2::])
+    for petrol in petrols:  #создаёт словарь с бензином и какие колонки есть
+        if petrol not in petrol_with_machine:
+            petrol_with_machine[petrol] = [int(machine)]
+        else:
+            petrol_with_machine[petrol].append(int(machine))
+
+    machine_with_max_quantity[int(machine)] = int(max_quantity) #создаёт словарь словарь с номером автомата + макс очередь
+
+print (petrol_with_machine)
+print(machine_with_max_quantity)
+
 for line in file:
     time, liter, petrol = line.split()
-    print(liter)
-    if station[]
+    hours, minutes = time.split(':')
+
+    print(time)
+
+
